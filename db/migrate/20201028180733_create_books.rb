2e581a2ef95b320e32f2b1f6b30ec076ee0a44ed
Guillermo <guillhermoherrera@gmail.com>
2020-10-28 12:22:29 -0600

    first endpoint book index
    rails g controller Books index
    rails g model Book titulo autor
    rails db:migrate

---
+++ db/migrate/20201028180733_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :titulo
+      t.string :autor
+
+      t.timestamps
+    end
+  end
+end
