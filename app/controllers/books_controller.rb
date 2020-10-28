a5bb12c7e4f1cb8d8aba06e7d3d33b1a7ca6f6d8
Guillermo <guillhermoherrera@gmail.com>
2020-10-28 13:30:41 -0600

    endpoint POST & params
    GET
    curl http://localhost:3000/books
    
    POST
    curl -d "book[titulo]: Mi Titulo" -d "book[autor]: Mi Autor" http://localhost:3000/books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,20 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    byebug
+    book = Book.new(book_params)
+    
+    if book.save
+      render json: book, status: :created
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def book_params
+    params.required(:book).permit(:titulo, :autor)
+  end
 end
